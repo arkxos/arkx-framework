fc182cdc0addd1571f8fb8199b4eef7ed4718abe
darkness <darkness_sky@qq.com>
2021-06-06 16:01:11 +0800

    增加PageData类，支持分页数据类型在Swagger中显示详细字段信息

---
+++ rapidark-server/components/common-core/src/main/java/me/zhengjie/utils/PageData.java
@@ -0,0 +1,23 @@
+package me.zhengjie.utils;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author darkness
+ * @version 1.0
+ * @date 2021/6/6 15:23
+ */
+@Data
+@ApiModel(value = "分页数据")
+public class PageData<T> {
+
+    @ApiModelProperty(required = true, value = "总记录数", example = "0", position=1)
+    private String totalElements;
+
+    @ApiModelProperty(required = true, value = "数据", position=2)
+    private List<T> content;
+}
