b50b296f49c6e6df47eae0f3cd4fdb463ae8d96d
Darkness <darkness_sky@qq.com>
2024-09-27 11:26:47 +0800

    重构redisTemplate配置

--- rapidark-server/components/common-starter/src/main/java/com/rapidark/autoconfigure/RedisCacheAutoConfiguration.java
+++ rapidark-server/components/common-starter/src/main/java/com/rapidark/autoconfigure/RedisCacheAutoConfiguration.java
@@ -33,6 +33,7 @@ import java.time.Duration;
 @Configuration
 @AutoConfigureAfter({RedisAutoConfiguration.class})
 public class RedisCacheAutoConfiguration {
+
     public RedisCacheAutoConfiguration() {
         log.info("init redis cache");
     }
@@ -47,16 +48,20 @@ public class RedisCacheAutoConfiguration {
     public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
         template.setConnectionFactory(factory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        // 解决key的序列化方式，使用String。
         RedisSerializer<String> stringSerializer = new StringRedisSerializer();
         // key采用String的序列化方式
         template.setKeySerializer(stringSerializer);
         // hash的key也采用String的序列化方式
         template.setHashKeySerializer(stringSerializer);
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
         // value序列化方式采用jackson
         template.setValueSerializer(jackson2JsonRedisSerializer);
         // hash的value序列化方式采用jackson
