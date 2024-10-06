49b3c7cd946ee691c21800961766ae6deb7d62aa
Darkness <darkness_sky@qq.com>
2024-10-06 14:56:10 +0800

    增加网关jpa配置

---
+++ rapidark-server/platform/api-spring-server/src/main/java/com/rapidark/cloud/gateway/server/configuration/RedissonConfig.java
@@ -0,0 +1,55 @@
+//package com.rapidark.cloud.gateway.server.configuration;
+//
+//import com.rapidark.common.utils.StringUtils;
+//import org.redisson.Redisson;
+//import org.redisson.api.RedissonClient;
+//import org.redisson.config.Config;
+//import org.redisson.config.SingleServerConfig;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author darkness
+// * @version 1.0
+// * @date 2022/5/31 11:37
+// */
+//@Configuration
+//public class RedissonConfig {
+//
+//    @Value("${spring.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.redis.port}")
+//    private String port;
+//
+////    @Value("${spring.redis.password}")
+////    private String password;
+//
+//    private int timeout = 3000;
+//
+//    private int connectionPoolSize = 64;
+//
+//    private int connectionMinimumIdleSize = 10;
+//
+//    /**
+//     * 单机配置
+//     *
+//     * @return
+//     */
+//    @Bean
+//    RedissonClient redissonSingle() {
+//        Config config = new Config();
+//        SingleServerConfig serverConfig = config.useSingleServer()
+//                .setAddress("redis://" + host + ":" + port)
+//                .setTimeout(timeout)
+//                .setConnectionPoolSize(connectionPoolSize)
+//                .setConnectionMinimumIdleSize(connectionMinimumIdleSize);
+//        //密码设置
+////        if (StringUtils.isNotBlank(password)) {
+////            serverConfig.setPassword(password);
+////        }
+//        return Redisson.create(config);
+//    }
+//}
+//
