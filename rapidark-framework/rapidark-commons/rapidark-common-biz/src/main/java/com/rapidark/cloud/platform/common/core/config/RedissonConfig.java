21f2d4fbd201df7070d7aa72cb28fdf6052dc754
Darkness <darkness_sky@qq.com>
2024-11-24 14:59:16 +0800

    修复网关启动失败的问题

--- rapidark-framework/rapidark-commons/rapidark-common-biz/src/main/java/com/rapidark/cloud/platform/common/core/config/RedissonConfig.java
+++ rapidark-framework/rapidark-commons/rapidark-common-biz/src/main/java/com/rapidark/cloud/platform/common/core/config/RedissonConfig.java
@@ -1,20 +1,23 @@
 package com.rapidark.cloud.platform.common.core.config;
 
-import com.rapidark.framework.common.utils.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 /**
  * @Author: linrongxin
  * @Date: 2019/9/19 17:49
  */
 @Configuration
+@ConditionalOnProperty(prefix = "spring.data.redisson", name = "enabled", havingValue = "true")
 public class RedissonConfig {
+
 	@Value("${spring.data.redis.host}")
 	private String host;
 
@@ -44,7 +47,7 @@ public class RedissonConfig {
 				.setConnectionPoolSize(connectionPoolSize)
 				.setConnectionMinimumIdleSize(connectionMinimumIdleSize);
 		//密码设置
-		if (StringUtils.isNotBlank(password)) {
+		if (StringUtils.hasText(password)) {
 			serverConfig.setPassword(password);
 		}
 		return Redisson.create(config);
