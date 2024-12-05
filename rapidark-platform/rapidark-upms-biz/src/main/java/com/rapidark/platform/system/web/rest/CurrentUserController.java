a5a18ade47c44f9b1e6c4cf6a4df9794d6455845
Darkness <darkness_sky@qq.com>
2024-12-05 12:49:00 +0800

    重构

---
+++ rapidark-platform/rapidark-upms-biz/src/main/java/com/rapidark/platform/system/web/rest/CurrentUserController.java
@@ -0,0 +1,167 @@
+package com.rapidark.platform.system.web.rest;
+
+import com.rapidark.cloud.base.client.model.AuthorityMenu;
+import com.rapidark.platform.system.api.entity.SysMenu;
+import com.rapidark.platform.system.api.entity.SysUser;
+import com.rapidark.cloud.base.server.service.BaseAuthorityService;
+import com.rapidark.cloud.base.server.service.SysMenuService;
+import com.rapidark.cloud.base.server.service.SysUserService;
+import com.rapidark.cloud.base.server.service.OpenAppService;
+import com.rapidark.platform.system.api.model.RouterVo;
+import com.rapidark.platform.system.service.RouterService;
+import com.rapidark.cloud.platform.common.security.util.SecurityUtils;
+import com.rapidark.framework.common.constants.CommonConstants;
+import com.rapidark.framework.common.exception.OpenAlertException;
+import com.rapidark.framework.common.model.ResponseResult;
+import com.rapidark.framework.common.security.OpenHelper;
+import com.rapidark.framework.common.security.OpenUserDetails;
+import com.rapidark.framework.common.utils.StringUtils;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+/**
+ * @author: liuyadu
+ * @date: 2019/5/24 13:31
+ * @description:
+ */
+@Schema(title = "当前登陆用户")
+@AllArgsConstructor
+@RestController
+public class CurrentUserController {
+
+	private final SysMenuService sysMenuService;
+	private final RouterService routerService;
+
+	private final SysUserService sysUserService;
+	private final BaseAuthorityService baseAuthorityService;
+	private final OpenAppService openAppService;
+	//    @Autowired
+//    private RedisTokenStore redisTokenStore;
+	private final SysMenuService menuService;
+
+	//	@Autowired
+//	private final PasswordEncoder passwordEncoder;
+
+	/**
+	 * 返回当前用户的树形菜单集合
+	 * @return 当前用户的树形菜单
+	 */
+	@GetMapping("/current/user/routers")
+	public ResponseResult<List<RouterVo>> ueryCurrentUserMenu() {
+		// 获取符合条件的菜单
+		Set<com.rapidark.platform.system.api.entity.SysMenu> all = new HashSet<>();
+		SecurityUtils.getRoles().forEach(roleId -> all.addAll(sysMenuService.findMenuByRoleId(roleId)));
+
+		List<com.rapidark.platform.system.api.entity.SysMenu> menus = new ArrayList<>(all);
+		menus.sort(Comparator.comparingLong(com.rapidark.platform.system.api.entity.SysMenu::getParentId).thenComparingInt(com.rapidark.platform.system.api.entity.SysMenu::getSortOrder));
+
+		return ResponseResult.ok(routerService.buildRouters(menus));
+	}
+
+	/**
+	 * 修改当前登录用户密码
+	 *
+	 * @return
+	 */
+	@Schema(title = "修改当前登录用户密码", name = "修改当前登录用户密码")
+	@PostMapping("/current/user/rest/password")
+	public ResponseResult restPassword(
+			@RequestParam(value = "oldPassword") String oldPassword,
+			@RequestParam(value = "password") String password,
+			@RequestParam(value = "confirmPassword") String confirmPassword
+	) {
+		OpenUserDetails user = OpenHelper.getUser();
+		Assert.notNull(user, "登录过期，请重新登录");
+		if (StringUtils.isBlank(password) || StringUtils.isBlank(confirmPassword) || !confirmPassword.equals(password)) {
+			throw new OpenAlertException("新密码与确认密码不一致");
+		}
+//		if (StringUtils.isBlank(oldPassword) || !passwordEncoder.matches(oldPassword, user.getPassword())) {
+//			throw new OpenAlertException("旧密码输入错误");
+//		}
+//		if (passwordEncoder.matches(password, user.getPassword())) {
+//			throw new OpenAlertException("新密码与旧密码不能相同");
+//		}
+		sysUserService.updatePassword(user.getUserId(), password);
+		return  ResponseResult.ok().msg("修改密码成功");
+	}
+
+	/**
+	 * 修改当前登录用户基本信息
+	 *
+	 * @param nickName
+	 * @param userDesc
+	 * @param avatar
+	 * @return
+	 */
+	@Schema(title = "修改当前登录用户基本信息", name = "修改当前登录用户基本信息")
+	@PostMapping("/current/user/update")
+	public ResponseResult updateUserInfo(
+			@RequestParam(value = "nickName") String nickName,
+			@RequestParam(value = "userDesc", required = false) String userDesc,
+			@RequestParam(value = "avatar", required = false) String avatar
+	) {
+		OpenUserDetails openUserDetails = OpenHelper.getUser();
+		Assert.notNull(openUserDetails, "登录过期，请重新登录");
+		SysUser user = new SysUser();
+		user.setUserId(openUserDetails.getUserId());
+		user.setNickName(nickName);
+		if (userDesc != null && !"".equals(userDesc)) {
+			user.setUserDesc(userDesc);
+		}
+		if (avatar != null && !"".equals(avatar)) {
+			user.setAvatar(avatar);
+		}
+		sysUserService.updateUser(user);
+		openUserDetails.setNickName(nickName);
+		openUserDetails.setAvatar(avatar);
+//        OpenHelper.updateOpenUser(redisTokenStore, openUserDetails);
+		return ResponseResult.ok();
+	}
+
+	/**
+	 * 获取登陆用户已分配权限
+	 *
+	 * @return
+	 */
+	@Schema(title = "获取当前登录用户已分配菜单权限", name = "获取当前登录用户已分配菜单权限")
+	@GetMapping("/current/user/menu")
+	public ResponseResult<List<AuthorityMenu>> findAuthorityMenu(@RequestParam(value = "serviceId", required = false) String serviceId) {
+		OpenUserDetails user = OpenHelper.getUser();
+		Assert.notNull(user, "登录过期，请重新登录");
+		if (StringUtils.isEmpty(serviceId)) {
+			// modify, add search menu with serviceId(appNameEn)
+			serviceId = openAppService.getAppClientInfo(user.getClientId()).getAdditionalInformation().get("appNameEn").toString();
+		}
+		serviceId = serviceId.trim();
+		List<AuthorityMenu> result = baseAuthorityService.findAuthorityMenuByUser(
+				user.getUserId(), CommonConstants.ROOT.equals(user.getUsername()), serviceId);
+		return ResponseResult.ok(result);
+	}
+
+	/**
+	 * 获取路由信息
+	 *
+	 * @return 路由信息
+	 */
+	@GetMapping("/current/user/routersx")
+	public ResponseResult<List<RouterVo>> getRouters() {
+		OpenUserDetails user = OpenHelper.getUser();
+		List<AuthorityMenu> menus = baseAuthorityService.findAuthorityMenuByUser(
+				user.getUserId(), CommonConstants.ROOT.equals(user.getUsername()), "");
+
+		List<SysMenu> sysMenus = new ArrayList<>();
+		for (AuthorityMenu menu : menus) {
+			sysMenus.add(menu);
+		}
+		List<RouterVo> routers = null;//menuService.buildRouters(baseMenus);
+		return ResponseResult.ok(routers);
+	}
+
+}
---
+++ rapidark-platform/rapidark-upms-biz/src/main/java/com/rapidark/platform/system/web/rest/SysDeptController.java
@@ -0,0 +1,155 @@
+/*
+ *
+ *      Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  Neither the name of the pig4cloud.com developer nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ *  Author: lengleng (wangiegie@gmail.com)
+ *
+ */
+
+package com.rapidark.platform.system.web.rest;
+
+import com.pig4cloud.plugin.excel.annotation.RequestExcel;
+import com.pig4cloud.plugin.excel.annotation.ResponseExcel;
+import com.rapidark.platform.system.api.entity.SysDept;
+import com.rapidark.platform.system.api.vo.DeptExcelVo;
+import com.rapidark.platform.system.service.SysDeptService;
+import com.rapidark.framework.common.model.ResponseResult;
+import com.rapidark.cloud.platform.common.log.annotation.SysLog;
+import com.rapidark.cloud.platform.common.security.annotation.HasPermission;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * <p>
+ * 部门管理 前端控制器
+ * </p>
+ *
+ * @author lengleng
+ * @since 2018-01-20
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping("/dept")
+@Tag(description = "dept", name = "部门管理模块")
+@SecurityRequirement(name = HttpHeaders.AUTHORIZATION)
+public class SysDeptController {
+
+	private final SysDeptService sysDeptService;
+
+	/**
+	 * 通过ID查询
+	 * @param id ID
+	 * @return SysDept
+	 */
+	@GetMapping("/{id}")
+	public ResponseResult getById(@PathVariable Long id) {
+		return ResponseResult.ok(sysDeptService.getById(id));
+	}
+
+	/**
+	 * 查询全部部门
+	 */
+	@GetMapping("/list")
+	public ResponseResult list() {
+		return ResponseResult.ok(sysDeptService.list());
+	}
+
+	/**
+	 * 返回树形菜单集合
+	 * @param deptName 部门名称
+	 * @return 树形菜单
+	 */
+	@GetMapping(value = "/tree")
+	public ResponseResult getTree(String deptName) {
+		return ResponseResult.ok(sysDeptService.selectTree(deptName));
+	}
+
+	/**
+	 * 添加
+	 * @param sysDept 实体
+	 * @return success/false
+	 */
+	@SysLog("添加部门")
+	@PostMapping
+	@HasPermission("sys_dept_add")
+	public ResponseResult save(@Valid @RequestBody SysDept sysDept) {
+		return ResponseResult.ok(sysDeptService.save(sysDept));
+	}
+
+	/**
+	 * 删除
+	 * @param id ID
+	 * @return success/false
+	 */
+	@SysLog("删除部门")
+	@DeleteMapping("/{id}")
+	@HasPermission("sys_dept_del")
+	public ResponseResult removeById(@PathVariable Long id) {
+		return ResponseResult.ok(sysDeptService.removeDeptById(id));
+	}
+
+	/**
+	 * 编辑
+	 * @param sysDept 实体
+	 * @return success/false
+	 */
+	@SysLog("编辑部门")
+	@PutMapping
+	@HasPermission("sys_dept_edit")
+	public ResponseResult update(@Valid @RequestBody SysDept sysDept) {
+		sysDept.setUpdateTime(LocalDateTime.now());
+		return ResponseResult.ok(sysDeptService.updateById(sysDept));
+	}
+
+	/**
+	 * 查收子级列表
+	 * @return 返回子级
+	 */
+	@GetMapping(value = "/getDescendantList/{deptId}")
+	public ResponseResult getDescendantList(@PathVariable Long deptId) {
+		return ResponseResult.ok(sysDeptService.listDescendant(deptId));
+	}
+
+	/**
+	 * 导出部门
+	 * @return
+	 */
+	@ResponseExcel
+	@GetMapping("/export")
+	public List<DeptExcelVo> export() {
+		return sysDeptService.listExcelVo();
+	}
+
+	/**
+	 * 导入部门
+	 * @param excelVOList
+	 * @param bindingResult
+	 * @return
+	 */
+	@PostMapping("import")
+	public ResponseResult importDept(@RequestExcel List<DeptExcelVo> excelVOList, BindingResult bindingResult) {
+
+		return sysDeptService.importDept(excelVOList, bindingResult);
+	}
+
+}
