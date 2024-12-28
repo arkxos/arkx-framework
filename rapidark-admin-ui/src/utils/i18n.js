135d50b96fd7a8bcddf0fcc27ad7bb295627bd14
Darkness <darkness_sky@qq.com>
2024-12-28 13:02:49 +0800

    删除ui

--- rapidark-admin-ui/src/utils/i18n.js
+++
@@ -1,4 +0,0 @@
-export function translateTitle(title) {
-  if (this.$te(`vabI18n.${title}`)) return this.$t(`vabI18n.${title}`)
-  return title
-}
--- rapidark-admin-ui/vab.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // 开发工具控制台名字显示
-  webpackBarName: 'RapidArk-Admin-Pro',
-  // 浏览器注释显示
-  webpackBanner:
-    ' build: RapidArk-Admin-Pro \n copyright: RapidArk rapidark@qq.com \n time: ',
-}
