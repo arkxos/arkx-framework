9e9945377afbb4a8e08f0e6511fc8ab6a0415e86
Darkness <darkness_sky@qq.com>
2025-05-13 21:58:56 +0800

    调整模块依赖，统一到dependencies

--- arkxos-framework-commons/arkxos-framework-commons-core/src/test/java/SqlParseTest.java
+++ arkxos-framework-commons/arkxos-framework-commons-core/src/test/java/SqlParseTest.java
@@ -1,47 +1,47 @@
-
-import java.util.List;
-
-import com.alibaba.druid.DbType;
-import com.alibaba.druid.sql.SQLUtils;
-import com.alibaba.druid.sql.ast.SQLStatement;
-import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
-import com.alibaba.druid.util.JdbcConstants;
-
-/**
- * @author Darkness
- * @date 2020年11月4日 下午5:57:47
- * @version V1.0
- */
-public class SqlParseTest {
-	
-	public static void main(String[] args) {
-        // String sql = "update t set name = 'x' where id < 100 limit 10";
-        // String sql = "SELECT ID, NAME, AGE FROM USER WHERE ID = ? limit 2";
-        // String sql = "select * from tablename limit 10";
-
-        String sql = "select t.*, u.*, t.id, t.order_id, u.name, u.field from emp_table t left join t_user u on t.id = u.id";
-        DbType dbType = JdbcConstants.MYSQL;
-
-        //格式化输出
-        String result = SQLUtils.format(sql, dbType);
-        System.out.println(result); // 缺省大写格式
-        List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, dbType);
-
-        //解析出的独立语句的个数
-        System.out.println("size is:" + stmtList.size());
-        for (int i = 0; i < stmtList.size(); i++) {
-
-            SQLStatement stmt = stmtList.get(i);
-            MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
-            stmt.accept(visitor);
-
-            //获取表名称
-//            System.out.println("Tables : " + visitor.g());
-            //获取操作方法名称,依赖于表名称
-            System.out.println("Manipulation : " + visitor.getTables());
-            //获取字段名称
-            System.out.println("fields : " + visitor.getColumns());
-        }
-    }
-
-}
+//
+//import java.util.List;
+//
+//import com.alibaba.druid.DbType;
+//import com.alibaba.druid.sql.SQLUtils;
+//import com.alibaba.druid.sql.ast.SQLStatement;
+//import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
+//import com.alibaba.druid.util.JdbcConstants;
+//
+///**
+// * @author Darkness
+// * @date 2020年11月4日 下午5:57:47
+// * @version V1.0
+// */
+//public class SqlParseTest {
+//
+//	public static void main(String[] args) {
+//        // String sql = "update t set name = 'x' where id < 100 limit 10";
+//        // String sql = "SELECT ID, NAME, AGE FROM USER WHERE ID = ? limit 2";
+//        // String sql = "select * from tablename limit 10";
+//
+//        String sql = "select t.*, u.*, t.id, t.order_id, u.name, u.field from emp_table t left join t_user u on t.id = u.id";
+//        DbType dbType = JdbcConstants.MYSQL;
+//
+//        //格式化输出
+//        String result = SQLUtils.format(sql, dbType);
+//        System.out.println(result); // 缺省大写格式
+//        List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, dbType);
+//
+//        //解析出的独立语句的个数
+//        System.out.println("size is:" + stmtList.size());
+//        for (int i = 0; i < stmtList.size(); i++) {
+//
+//            SQLStatement stmt = stmtList.get(i);
+//            MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
+//            stmt.accept(visitor);
+//
+//            //获取表名称
+////            System.out.println("Tables : " + visitor.g());
+//            //获取操作方法名称,依赖于表名称
+//            System.out.println("Manipulation : " + visitor.getTables());
+//            //获取字段名称
+//            System.out.println("fields : " + visitor.getColumns());
+//        }
+//    }
+//
+//}
