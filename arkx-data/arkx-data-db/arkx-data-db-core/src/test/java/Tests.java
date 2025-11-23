5877df201a9ca574c9afcae0368c1c3d367a3ca1
Nobody <darkness_sky@qq.com>
2025-11-23 11:06:14 +0800

    修复编译打包错误

--- arkx-data/arkx-data-db/arkx-data-db-core/src/test/java/Tests.java
+++ arkx-data/arkx-data-db/arkx-data-db-core/src/test/java/Tests.java
@@ -1,24 +1,24 @@
-import com.kingbase8.core.Provider;
-import com.kingbase8.jdbc.TimestampUtils;
-
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.TimeZone;
-
-/**
- * @author Nobody
- * @date 2025-07-09 16:47
- * @since 1.0
- */
-public class Tests {
-
-    public static void main(String[] args) throws SQLException {
-        String time = "2020-11-23 0:00:00";
-        time = "2020-12-23 17:09:51.693";
-        Provider<TimeZone> timeZoneProvider = () -> TimeZone.getDefault(); // 提供一个默认的时区提供者
-        TimestampUtils timestampUtils = new TimestampUtils(false, timeZoneProvider);
-
-        Timestamp result = timestampUtils.toTimestamp(null, time);
-        System.out.println(result);
-    }
-}
+//import com.kingbase8.core.Provider;
+//import com.kingbase8.jdbc.TimestampUtils;
+//
+//import java.sql.SQLException;
+//import java.sql.Timestamp;
+//import java.util.TimeZone;
+//
+///**
+// * @author Nobody
+// * @date 2025-07-09 16:47
+// * @since 1.0
+// */
+//public class Tests {
+//
+//    public static void main(String[] args) throws SQLException {
+//        String time = "2020-11-23 0:00:00";
+//        time = "2020-12-23 17:09:51.693";
+//        Provider<TimeZone> timeZoneProvider = () -> TimeZone.getDefault(); // 提供一个默认的时区提供者
+//        TimestampUtils timestampUtils = new TimestampUtils(false, timeZoneProvider);
+//
+//        Timestamp result = timestampUtils.toTimestamp(null, time);
+//        System.out.println(result);
+//    }
+//}
